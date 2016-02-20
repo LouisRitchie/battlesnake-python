23e5004d8c552a8bce33687c09bfdb73a0a90aa7
Louis Ritchie <louiscritchie@gmail.com>
2016-02-20 23:12:37 +0000

    fixing 503 error...

--- app/main.py
+++ app/main.py
@@ -9,14 +9,14 @@ def static(path):
 
 @bottle.get('/')
 def index():
-    head_url = '%s://%s/static/head.png' % (
+    head_url = 'http://i.imgur.com/tWoo7jR.png' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
 
     return {
         'color': '#00ff77',
-        'head': http://i.imgur.com/tWoo7jR.png
+        'head': head_url
     }
 
 
