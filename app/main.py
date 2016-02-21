5360b40d7c06a3c757d0a32eff977aaee23af779
Louis Ritchie <louiscritchie@gmail.com>
2016-02-21 00:01:29 +0000

    Added %s %s to head_url string

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def static(path):
 
 @bottle.get('/')
 def index():
-    head_url = 'http://i.imgur.com/tWoo7jR.png' % (
+    head_url = '%s %s http://i.imgur.com/tWoo7jR.png' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
